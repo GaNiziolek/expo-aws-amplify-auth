dbc96eb9a5b7c446eb6957755cb54b986d6156e3
Gabriel Niziolek <ganiziolek@gmail.com>
2023-12-26 14:57:15 -0300

    trying to setup again

--- auth_app/App.js
+++ auth_app/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { Button } from 'react-native';
-
-import { Amplify } from 'aws-amplify';
-import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
-
-import awsExports from './amplifyconfiguration.json';
-Amplify.configure(awsExports);
-
-function SignOutButton() {
-  const { signOut } = useAuthenticator();
-  return <Button title="Sign Out" onPress={signOut} />;
-}
+import { View, Text } from 'react-native';
 
 function App() {
   return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <SignOutButton />
-      </Authenticator>
-    </Authenticator.Provider>
-  );
+    <View>
+        <Text>asd</Text>
+    </View>
+  )
 }
 
 export default App;
